dc5c8e0ceee71852cdca542f18ad0da1fbe899f8
Eddy Jo <edwardjo811@gmail.com>
2019-01-26 18:22:25 -0500

    practice rendering classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1
   };
 
   render() {
     return (
       <div>
-        <span style={ { fontSize: 30 } } className="badge badge-primary m-2">{ this.formatCount() }</span>
+        <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
