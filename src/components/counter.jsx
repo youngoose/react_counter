3d08005a4f46486327db62c05458df9c0385b631
Eddy Jo <edwardjo811@gmail.com>
2019-01-26 17:43:38 -0500

    practice styling with state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 0
   };
 
   render() {
     return (
-      <React.Fragment>
-        <span>{ this.formatCount() }</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span style={ { fontSize: 30 } } className="badge badge-primary m-2">{ this.formatCount() }</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
     );
   }
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    return count === 0 ? 'Zero' : count;
   }
 }
  
