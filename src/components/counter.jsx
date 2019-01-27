1f2fc46b3c883f6067cd5bf44ca8d9584c662640
Eddy Jo <edwardjo811@gmail.com>
2019-01-27 17:38:10 -0500

    practice method rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,28 +2,25 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 1,
+    tags: ['1', '2']
   };
+
+  renderTags() {
+    if(this.state.tags.length === 0) return <p>there is no tags</p>;
+
+    return <ul>{ this.state.tags.map(tag => <li key={ tag }>{ tag }</li>) }</ul>
+  }
 
   render() {
     return (
-      <div>
-        <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      </div>
-    );
+    <div>
+      { this.state.tags.length === 0 && "Please create a new tag!"}
+      { this.renderTags() }
+    </div>
+    )
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
-  }
 }
  
 export default Counter;
