a9b9645ec41679c96517602e6f47714cf26d8149
Eddy Jo <edwardjo811@gmail.com>
2019-02-10 10:18:41 -0500

    practice event handler parameter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,22 +11,26 @@ class Counter extends Component {
   //   this.handleIncrement = this.handleIncrement.bind(this); 
   // }
 
-  handleIncrement = () => {
+  handleIncrement = product => {
     // ** important: fat arrow function resolves "this" keyword binding issue without super keyword
     // now we don't need to do rebinding the event handler everytime manually 
     // plz take a note that this shortcut function would be got rid of in the future from ES6 or not
     // then use the former apporoach like above with super
     // amazing man..
-    console.log("Increment Clicked", this);
     // obj.method();
     // function();
+    console.log(product)
+    this.setState({ count : this.state.count + 1 }); 
   }
 
   render() {
+    const product = { id: 1 };
     return (
       <div>
         <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
-        <button onClick={ this.handleIncrement } className="btn btn-secondary btn-sm">Increment</button>
+        <button 
+          onClick={ () => this.handleIncrement(product)} 
+          className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
