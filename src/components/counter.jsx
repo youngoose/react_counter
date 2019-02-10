aeb03308318c0d1afd3f80eb358b6dd21d216cec
Eddy Jo <edwardjo811@gmail.com>
2019-02-10 14:01:07 -0500

    added navbar to app.js

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,49 +1,31 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3" ]
-  };
-
-  // constructor() {
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this); 
-  // }
-
-  handleIncrement = product => {
-    // ** important: fat arrow function resolves "this" keyword binding issue without super keyword
-    // now we don't need to do rebinding the event handler everytime manually 
-    // plz take a note that this shortcut function would be got rid of in the future from ES6 or not
-    // then use the former apporoach like above with super
-    // amazing man..
-    // obj.method();
-    // function();
-    console.log(product)
-    this.setState({ count : this.state.count + 1 }); 
-  }
-
   render() {
-    const product = { id: 1 };
     return (
       <div>
         <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
         <button 
-          onClick={ () => this.handleIncrement(product)} 
-          className="btn btn-secondary btn-sm">Increment</button>
+          onClick={ () => this.props.onIncrement(this.props.counter) } 
+          className="btn btn-secondary btn-sm">Increment
+        </button>
+        <button 
+          onClick={ () => this.props.onDelete(this.props.counter.id) }
+          className="btn btn-danger btn-sm m-2">Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += (this.props.counter.value === 0) ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    const { value } = this.props.counter;
+    return value === 0 ? 'Zero' : value;
   }
 }
  
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,15 +4,52 @@ import Counter from './counter';
 class Counters extends Component {
   state = { 
     counters: [
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 }
     ]
+   };
+
+   handleIncrement = counter => {
+     const counters = [...this.state.counters];
+     const index = counters.indexOf(counter);
+     counters[index] = {...counter};
+     counters[index].value++;
+     this.setState({ counters });
    }
+
+    handleDelete = counterId => {
+      const counters = this.state.counters.filter(c => c.id !== counterId);
+      this.setState({ counters });
+    };
+
+    handleReset = () => {
+      const counters = this.state.counters.map(c => {
+        c.value = 0;
+        return c;
+      });
+      this.setState({ counters });
+    };
+
   render() { 
-    return (<div>
-     { this.state.counters.map(counter => <Counter key={ counter.id }/>)}
+    return (
+    <div>
+     <button 
+        onClick={ this.handleReset }
+        className="btn-primary btn-sm m-2">
+        Reset
+     </button>
+     { this.state.counters.map(counter => 
+      <Counter 
+        key={ counter.id }
+        onReset={ this.handleReset }
+        onDelete={ this.handleDelete }
+        onIncrement={ this.handleIncrement }
+        counter={ counter }
+      />
+      )
+     }
     </div>);
   }
 }
